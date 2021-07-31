c30ab523d81cf0cafd1a22ff6255fba8bfa68f97
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-07-31 22:58:38 +0530

    Activity-6

--- Activity_06.py
+++ Activity_06.py
@@ -1,10 +1,14 @@
 my_list = input().split()
 new_list = my_list[0:3]
-print(my_list)
-print(new_list)
+print("sliced list = ", new_list)
 my_list[0] = 0
 new_list[0] = 0
 my_list[-1] = 0
 new_list[-1] = 0
-print(my_list)
-print(new_list)
+print("replaced list-1 = ", my_list)
+print("replaced list-2 = ",new_list)
+
+# Oh My God ! 1
+# sliced list =  ['Oh', 'My', 'God']
+# replaced list-1 =  [0, 'My', 'God', '!', 0]
+# replaced list-2 =  [0, 'My', 0]
