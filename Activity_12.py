ab99ead79e571936976fe4145e85ca81b59ad4d3
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-08-01 17:51:32 +0530

    Activity-12

---
+++ Activity_12.py
@@ -0,0 +1,23 @@
+def input_numbers():
+    line = input().split()
+    numbers = [int(i) for i in line]
+    return numbers
+
+def maxi(n1, n2, n3):
+    if n1 > n2:
+        if n1 > n3:
+            return n1
+        return n3
+    elif n2 > n3:
+        return n2
+    return n3
+
+def display(n1, n2, n3, min):
+    print(f"{min} is the greatest number among {n1}, {n2} and {n3}.")
+
+def main():
+    a, b, c = input_numbers()
+    min = maxi(a, b, c)
+    display(a, b, c, min)
+
+main()
