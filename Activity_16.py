7ed5f4ee3d53d68e76cbdc105fc6bce5507707cd
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-08-01 20:53:10 +0530

    Activity-17

--- Activity_16.py
+++ Activity_16.py
@@ -1,13 +1,12 @@
-def get_cs():
+def get_lot():
     return input()
 
 
-def cs_to_lot(string):
-    separated = string.split(';')
-    listOt = []
-    for i in separated:
-        listOt.append(tuple(i.split('=')))
-    return listOt
+def lot_to_cs(listOfTuples):
+    string = ""
+    for (a, b) in listOfTuples:
+        string += a+"="+b+";"
+    return string
 
 
 def display(l):
@@ -15,8 +14,8 @@ def display(l):
 
 
 def main():
-    cs = get_cs()
-    lot = cs_to_lot(cs)
+    cs = get_lot()
+    lot = lot_to_cs(cs)
     display(lot)
 
 
