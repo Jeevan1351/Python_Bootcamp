c9940ce585e0eeea01ab95a06979ffe5c9b85090
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-08-01 19:20:10 +0530

    Activity-15

---
+++ Activity_15.py
@@ -0,0 +1,14 @@
+def words():
+    w = input().split()
+    c_w = [word for word in w]
+    return w, c_w
+
+
+def main():
+    wrds, c_words = words()
+    wrds.sort()
+    print(wrds)
+    print(c_words)
+    print(sorted(c_words))
+
+main()
