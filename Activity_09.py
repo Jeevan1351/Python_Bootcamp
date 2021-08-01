bd3dc6aa6b81b05b66c64cb7b8c57bd9f20edb6b
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-08-01 16:43:57 +0530

    Activity_09 fix

--- Activity_09.py
+++ Activity_09.py
@@ -1,11 +1,11 @@
 l = float(input("Enter the length of the tromboloid: "))
 b = float(input("Enter the breadth of the tromboloid: "))
 h = float(input("Enter the height of the tromboloid: "))
-k = float(input("Enter the Characteristic constant of the tromboloid: "))
+k = (l+b+h)**2
 
 volume = h**2 * b**2 / k**0.5
 
-r = 3 / 4 * (volume**(1/3))
+r = (3/4 * volume)**(1/3)
 
-print(f"Volume of tromboloid: {round(volume, 3)}")
-print(f"The radius of sphere with equivalent volume is: {round(r, 3)}")
+print("Volume of tromboloid: {:.3f}".format(round(volume, 3)))
+print("The radius of sphere with equivalent volume is: {:.3f}".format(round(r, 3)))
